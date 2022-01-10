522319380efa742399db3c5bc54fb11e16d43821
pointerboy <lukatrbovic@outlook.com>
2022-01-10 18:50:03 +0100

    Finally patched up the package versions

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { View, Text, useState, useEffect, Suspense } from "react";
+import { AppLoading } from "expo";
+import { YellowBox } from "react-native";
+import * as Font from "expo-font";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import ReduxThunk from "redux-thunk";
+
+import "firebase/firestore";
+import * as Facebook from "expo-facebook";
+import i18n from "i18n-js";
+import "react-native-gesture-handler";
+
+import userReducer from "./store/reducers/user";
+import postsReducer from "./store/reducers/posts";
+import categoriesReducer from "./store/reducers/categories";
+import { loadLanguageSetting } from "./shared/storage";
+import Loader from "./components/UI/Loader";
+import { API_KEY } from "env";
+
+import StartupScreen from "./screens/StartupScreen";
+
+import firebase from 'firebase'
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBBOAs3WcB8pipkcnF1Ng6Ztk3e0fBVMfs",
+
+  authDomain: "lostandfound-859a8.firebaseapp.com",
+
+  databaseURL: "https://lostandfound-859a8-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "lostandfound-859a8",
+
+  storageBucket: "lostandfound-859a8.appspot.com",
+
+  messagingSenderId: "604456261194",
+
+  appId: "1:604456261194:web:78a9f55795aaaa891778b3",
+
+  measurementId: "G-S4QGCWS6Y7"
+
+};
+
+// remove setTimeout() warning
+YellowBox.ignoreWarnings(["Setting a timer"]);
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+i18n.translations = {
+  en: require("./locales/en.json"),
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  posts: postsReducer,
+  categories: categoriesReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    kanit: require("./assets/fonts/Kanit-Regular.ttf"),
+    "kanit-light": require("./assets/fonts/Kanit-Light.ttf"),
+    "kanit-bold": require("./assets/fonts/Kanit-Bold.ttf"),
+  });
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <Suspense fallback={<Loader visible={true} alpha={1} />}>
+        <StartupScreen />
+      </Suspense>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
